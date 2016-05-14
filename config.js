812a88b40e59f6e92566c00d84f7c77dfb53aae5
Jorge <jorge.arcoma@gmail.com>
2016-05-14 17:31:07 +0200

    meh

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://blog.leoskimdo.com.com',
+        url: 'http://blog.leoskimdo.com',
         mail: {},
         database: {
             client: 'sqlite3',
